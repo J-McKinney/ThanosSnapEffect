245747b6c46dac3381bfe49d9819fceb93caab33
Jesse <jessemckinney513@gmail.com>
2021-08-17 22:26:57 -0400

    Updates

---
+++ precache-manifest.20367e1e1871fa6551c924c033523a2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b62c3529bf564840f931eb8475f3b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5e404605e900cd38ff5",
+    "url": "/static/css/main.b657c6a6.chunk.css"
+  },
+  {
+    "revision": "c65c4a67006872da68dd",
+    "url": "/static/js/2.72217ddb.chunk.js"
+  },
+  {
+    "revision": "c01abcb276b701885a9ef012faa8ee94",
+    "url": "/static/js/2.72217ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e404605e900cd38ff5",
+    "url": "/static/js/main.6b23f3ad.chunk.js"
+  },
+  {
+    "revision": "2da1e13c4765d47bc405",
+    "url": "/static/js/runtime-main.35432d51.js"
+  },
+  {
+    "revision": "a34f556e6385eb597fba081a5851bcd0",
+    "url": "/static/media/gauntlet-snap.a34f556e.gif"
+  },
+  {
+    "revision": "9cd83ab141f46852d16dd4b24ae69e19",
+    "url": "/static/media/image.9cd83ab1.png"
+  }
+]);
